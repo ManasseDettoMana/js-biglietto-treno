abded617889bcdc4afec7104160758e0eaf0284d
ManasseDettoMana <smarrazzomanasse@gmail.com>
2021-08-05 14:08:32 +0200

    refactoring

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 let chilometri = parseInt(prompt("Quanti chilometri intende percorrere?"));
 let eta = parseInt(prompt("Quanti hanni ha?"));
 
-document.getElementById("chilometri").innerHTML = chilometri
+document.getElementById("chilometri").innerHTML = chilometri;
 document.getElementById("eta").innerHTML = eta;
 
 
@@ -16,4 +16,3 @@ else if(eta>65){
 }
 
 document.getElementById("prezzo_biglietto").innerHTML = prezzoBiglietto.toFixed(2);
-console.log("il prezzo del biglietto è di: " + prezzoBiglietto);
